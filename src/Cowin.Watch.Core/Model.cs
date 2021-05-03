ee8d8b40a1154938373dd7f6459510a12cef423e
Nikhil <nikhs.j@gmail.com>
2021-05-04 01:04:33 +0530

    feat: add base models and tests for json

---
+++ src/Cowin.Watch.Core/Model.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Cowin.Watch.Core
+{
+    // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+    public class VaccineFee
+    {
+        [JsonPropertyName("vaccine")]
+        public string Vaccine { get;  set; }
+
+        [JsonPropertyName("fee")]
+        public string Fee { get;  set; }
+    }
+
+    public class Session
+    {
+        [JsonPropertyName("session_id")]
+        public string SessionId { get;  set; }
+
+        [JsonPropertyName("date")]
+        public string Date { get;  set; }
+
+        [JsonPropertyName("available_capacity")]
+        public int AvailableCapacity { get;  set; }
+
+        [JsonPropertyName("min_age_limit")]
+        public int MinAgeLimit { get;  set; }
+
+        [JsonPropertyName("vaccine")]
+        public string Vaccine { get;  set; }
+
+        [JsonPropertyName("slots")]
+        public IReadOnlyList<string> Slots { get;  set; }
+    }
+
+    public class Center
+    {
+        [JsonPropertyName("center_id")]
+        public int CenterId { get;  set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get;  set; }
+
+        [JsonPropertyName("name_l")]
+        public string NameL { get;  set; }
+
+        [JsonPropertyName("state_name")]
+        public string StateName { get;  set; }
+
+        [JsonPropertyName("state_name_l")]
+        public string StateNameL { get;  set; }
+
+        [JsonPropertyName("district_name")]
+        public string DistrictName { get;  set; }
+
+        [JsonPropertyName("district_name_l")]
+        public string DistrictNameL { get;  set; }
+
+        [JsonPropertyName("block_name")]
+        public string BlockName { get;  set; }
+
+        [JsonPropertyName("block_name_l")]
+        public string BlockNameL { get;  set; }
+
+        [JsonPropertyName("pincode")]
+        public string Pincode { get;  set; }
+
+        [JsonPropertyName("lat")]
+        public double Lat { get;  set; }
+
+        [JsonPropertyName("long")]
+        public double Long { get;  set; }
+
+        [JsonPropertyName("from")]
+        public string From { get;  set; }
+
+        [JsonPropertyName("to")]
+        public string To { get;  set; }
+
+        [JsonPropertyName("fee_type")]
+        public string FeeType { get;  set; }
+
+        [JsonPropertyName("vaccine_fees")]
+        public IReadOnlyList<VaccineFee> VaccineFees { get;  set; }
+
+        [JsonPropertyName("sessions")]
+        public IReadOnlyList<Session> Sessions { get;  set; }
+    }
+
+    public class Root
+    {
+        [JsonPropertyName("centers")]
+        public IReadOnlyList<Center> Centers { get;  set; }
+    }
+
+
+}
--- tests/Cowin.Watch.Core.Tests/JsonParseTests.cs
+++ tests/Cowin.Watch.Core.Tests/JsonParseTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Text.Json;
 
 namespace Cowin.Watch.Core.Tests
 {
@@ -6,9 +8,87 @@ namespace Cowin.Watch.Core.Tests
     public class JsonParseTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Centers_Are_Identified()
         {
+            var src = SampleJsonFactory.GetCentersApiResponseJson();
+            var root = JsonParser.DeserializeJson(src);
+
+            Assert.IsNotNull(root.Centers);
         }
+
+        [TestMethod]
+        public void Number_Of_Centers_Found_Is_Correct()
+        {
+            var src = SampleJsonFactory.GetCentersApiResponseJson();
+            var root = JsonParser.DeserializeJson(src);
+
+            var expectedCentersCount = 1;
+            Assert.AreEqual(root.Centers.Count, expectedCentersCount);
+        }
+
+        [TestMethod]
+        public void Center_District_Is_Correct()
+        {
+            var src = SampleJsonFactory.GetCentersApiResponseJson();
+            var root = JsonParser.DeserializeJson(src);
+
+            var expectedDistrictName = "Satara";
+            Center center = root.Centers.First();
+            Assert.AreEqual(center.DistrictName, expectedDistrictName);
+        }
+
+        [TestMethod]
+        public void Center_Sessions_Are_Identified()
+        {
+            var src = SampleJsonFactory.GetCentersApiResponseJson();
+            var root = JsonParser.DeserializeJson(src);
+
+            Center center = root.Centers.First();
+            Assert.IsNotNull(center.Sessions);
+        }
+
+        [TestMethod]
+        public void Number_Of_Center_Sessions_Found_Is_Correct()
+        {
+            var src = SampleJsonFactory.GetCentersApiResponseJson();
+            var root = JsonParser.DeserializeJson(src);
+
+            var expectedSessionCount = 1;
+            Center center = root.Centers.First();
+            Assert.AreEqual(center.Sessions.Count, expectedSessionCount);
+        }
+
+        [TestMethod]
+        public void Ensure_Vaccine_Name_Is_Expected()
+        {
+            var src = SampleJsonFactory.GetCentersApiResponseJson();
+            var root = JsonParser.DeserializeJson(src);
+
+            var expectedVaccineNames = new string[] { "COVISHIELD", "COVAXIN" };
+            bool IsVaccineValid(string vaccine)
+            {
+                return expectedVaccineNames.Any(v => v.Equals(vaccine, System.StringComparison.OrdinalIgnoreCase));
+            }
+            var vaccine = root.Centers.First().Sessions.First().Vaccine;
+            Assert.IsTrue(IsVaccineValid(vaccine));
+        }
+
+        [TestMethod]
+        public void Center_Sessions_Slots_Is_Not_Empty()
+        {
+            var src = SampleJsonFactory.GetCentersApiResponseJson();
+            var root = JsonParser.DeserializeJson(src);
+
+            var slots = root.Centers.First().Sessions.First().Slots;
+            Assert.IsNotNull(slots);
+        }
+    }
+
+
+    static class JsonParser
+    {
+        public static Root DeserializeJson(string json)
+            => JsonSerializer.Deserialize<Root>(json);
     }
 
     static class SampleJsonFactory
@@ -17,39 +97,39 @@ namespace Cowin.Watch.Core.Tests
         {
             return @"
 {
-  'centers': [
+  ""centers"": [
     {
-      'center_id': 1234,
-      'name': 'District General Hostpital',
-      'name_l': '',
-      'state_name': 'Maharashtra',
-      'state_name_l': '',
-      'district_name': 'Satara',
-      'district_name_l': '',
-      'block_name': 'Jaoli',
-      'block_name_l': '',
-      'pincode': '413608',
-      'lat': 28.7,
-      'long': 77.1,
-      'from': '09:00:00',
-      'to': '18:00:00',
-      'fee_type': 'Free',
-      'vaccine_fees': [
+      ""center_id"": 1234,
+      ""name"": ""District General Hostpital"",
+      ""name_l"": """",
+      ""state_name"": ""Maharashtra"",
+      ""state_name_l"": """",
+      ""district_name"": ""Satara"",
+      ""district_name_l"": """",
+      ""block_name"": ""Jaoli"",
+      ""block_name_l"": """",
+      ""pincode"": ""413608"",
+      ""lat"": 28.7,
+      ""long"": 77.1,
+      ""from"": ""09:00:00"",
+      ""to"": ""18:00:00"",
+      ""fee_type"": ""Free"",
+      ""vaccine_fees"": [
         {
-          'vaccine': 'COVISHIELD',
-          'fee': '250'
+          ""vaccine"": ""COVISHIELD"",
+          ""fee"": ""250""
         }
       ],
-      'sessions': [
+      ""sessions"": [
         {
-          'session_id': '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-          'date': '31-05-2021',
-          'available_capacity': 50,
-          'min_age_limit': 18,
-          'vaccine': 'COVISHIELD',
-          'slots': [
-            'FORENOON',
-            'AFTERNOON'
+          ""session_id"": ""3fa85f64-5717-4562-b3fc-2c963f66afa6"",
+          ""date"": ""31-05-2021"",
+          ""available_capacity"": 50,
+          ""min_age_limit"": 18,
+          ""vaccine"": ""COVISHIELD"",
+          ""slots"": [
+            ""FORENOON"",
+            ""AFTERNOON""
           ]
         }
       ]
