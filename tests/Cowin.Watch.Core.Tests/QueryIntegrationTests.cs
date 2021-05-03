36a243e6c245d1eab70b61f45ef2a570513dacac
Nikhil <nikhs.j@gmail.com>
2021-05-04 03:49:02 +0530

    test: Add integration test with live server
    Add base adress for queytests

---
+++ tests/Cowin.Watch.Core.Tests/QueryIntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using Cowin.Watch.Core.ApiClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cowin.Watch.Core.Tests
+{
+    [TestClass]
+    public class QueryIntegrationTests
+    {
+
+        [TestMethod]
+        public async Task When_Request_Is_Valid_Endpoint_Returns_200()
+        {
+            var client = LiveClientFactory.GetClient();
+            var districtId = 15; var dateFrom = DateTimeOffset.Parse("04-May-2021");
+
+            var response = await client.GetSessionsForDistrictAndDateAsync(districtId, dateFrom);
+            Assert.IsNotNull(response);
+        }
+    }
+
+    public class LiveClientFactory
+    {
+        public static ICowinApiClient GetClient() =>
+            new CowinApiHttpClient(new System.Net.Http.HttpClient()
+            {
+                BaseAddress = new Uri("https://cdn-api.co-vin.in/api/v2/"),
+                Timeout = TimeSpan.FromSeconds(30)
+            });
+    }
+}
--- tests/Cowin.Watch.Core.Tests/QueryTests.cs
+++ tests/Cowin.Watch.Core.Tests/QueryTests.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Cowin.Watch.Core.Tests
 {
@@ -24,14 +25,14 @@ namespace Cowin.Watch.Core.Tests
         }
 
         [TestMethod]
-        public async void Does_Client_Handle_Valid_Content()
+        public async Task Does_Client_Handle_Valid_Content()
         {
             var districtId = 15; var dateFrom = DateTimeOffset.Parse("04-May-2021");
             string content = SampleJsonFactory.GetCentersApiResponseJson();
             var cowinApiClient = ClientFactory.GetHandlerFor_200(content);
             
-            Root actualResponse = await cowinApiClient.GetSessionsForDistrictAndDateAsync(districtId, dateFrom);
             Root expectedResponse = JsonSerializer.Deserialize<Root>(content);
+            Root actualResponse = await cowinApiClient.GetSessionsForDistrictAndDateAsync(districtId, dateFrom);
 
             Assert.AreEqual<Root>(expectedResponse, actualResponse);
         }
@@ -40,12 +41,19 @@ namespace Cowin.Watch.Core.Tests
     public static class ClientFactory
     {
         public static ICowinApiClient GetCowinClientForHandler(DelegatingHandler delegatingHandler) => 
-            new CowinApiHttpClient(new HttpClient(delegatingHandler));
+            new CowinApiHttpClient(GetDefaultHttpClient(delegatingHandler));
 
         public static ICowinApiClient GetHandlerFor_204() =>
-            new CowinApiHttpClient(new HttpClient(new NoContentResponseHandler()));
+            new CowinApiHttpClient(GetDefaultHttpClient(new NoContentResponseHandler()));
 
         public static ICowinApiClient GetHandlerFor_200<TContent>(TContent content) =>
-            new CowinApiHttpClient(new HttpClient(OkResponseHandler<TContent>.ForContent<TContent>(content)));
+            new CowinApiHttpClient(GetDefaultHttpClient(OkResponseHandler<TContent>.ForContent<TContent>(content)));
+
+        public static HttpClient GetDefaultHttpClient(HttpMessageHandler httpMessageHandler) =>
+            new HttpClient(httpMessageHandler)
+            {
+                BaseAddress = new Uri("http://localhost/")
+            };
+
     }
 }
