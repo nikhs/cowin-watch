f4eb032d19519d2bd03f3095cce2d577f75cab61
Nikhil <nikhs.j@gmail.com>
2021-05-06 02:31:56 +0530

    test: add tests for Cowin Watch function

--- tests/Cowin.Watch.Core.Tests/Lib/SampleJsonFactory.cs
+++ tests/Cowin.Watch.Core.Tests/Lib/SampleJsonFactory.cs
@@ -1,7 +1,118 @@
-﻿namespace Cowin.Watch.Core.Tests.Lib
+﻿using System;
+
+namespace Cowin.Watch.Core.Tests.Lib
 {
     public static class SampleJsonFactory
     {
+        public static string GenerateResponseForVaccineWithSlots(VaccineType vaccineType)
+        {
+            return GenerateResponseWithSessions("District General Hostpital", vaccineType, 50, DateTimeOffset.Now);
+        }
+
+        public static string GenerateResponseForHospitalAndVaccineWithSlots(string hospitalName, VaccineType vaccineType)
+        {
+            return GenerateResponseWithSessions(hospitalName, vaccineType, 50, DateTimeOffset.Now);
+        }
+
+        public static string GenerateResponseWithSessions(string hospitalName, VaccineType vaccineType, int capacity, DateTimeOffset sessionDate)
+        {
+            string rawJson = @"
+{
+  ""centers"": [
+    {
+      ""center_id"": 1234,
+      ""name"": ""##_HOSPITAL_##"",
+      ""name_l"": """",
+      ""state_name"": ""Maharashtra"",
+      ""state_name_l"": """",
+      ""district_name"": ""Satara"",
+      ""district_name_l"": """",
+      ""block_name"": ""Jaoli"",
+      ""block_name_l"": """",
+      ""pincode"": 413608,
+      ""lat"": 28.7,
+      ""long"": 77.1,
+      ""from"": ""09:00:00"",
+      ""to"": ""18:00:00"",
+      ""fee_type"": ""Free"",
+      ""vaccine_fees"": [
+        {
+          ""vaccine"": ""##_Vaccine_##"",
+          ""fee"": ""250""
+        }
+      ],
+      ""sessions"": [
+        {
+          ""session_id"": ""3fa85f64-5717-4562-b3fc-2c963f66afa6"",
+          ""date"": ""##_Date_##"",
+          ""available_capacity"": ##_Capacity_##,
+          ""min_age_limit"": 18,
+          ""vaccine"": ""##_Vaccine_##"",
+          ""slots"": [
+            ""FORENOON"",
+            ""AFTERNOON""
+          ]
+        }
+      ]
+    }
+  ]
+}
+";
+            return rawJson
+                .Replace("##_HOSPITAL_##", hospitalName)
+                .Replace("##_Vaccine_##", vaccineType.ToString())
+                .Replace("##_Capacity_##", capacity.ToString())
+                .Replace("##_Date_##", sessionDate.ToString("d"));
+        }
+
+        public static string GenerateResponseForVaccineWithoutSlots(VaccineType vaccineType)
+        {
+            return GenerateResponseWithoutSessions("District General Hostpital", vaccineType);
+        }
+
+        public static string GenerateResponseForHospitalAndVaccineWithoutSlots(string hospitalName, VaccineType vaccineType)
+        {
+            return GenerateResponseWithoutSessions(hospitalName, vaccineType);
+        }
+
+        private static string GenerateResponseWithoutSessions(string hospitalName, VaccineType vaccineType)
+        {
+            string rawJson = @"
+{
+  ""centers"": [
+    {
+      ""center_id"": 1234,
+      ""name"": ""##_HOSPITAL_##"",
+      ""name_l"": """",
+      ""state_name"": ""Maharashtra"",
+      ""state_name_l"": """",
+      ""district_name"": ""Satara"",
+      ""district_name_l"": """",
+      ""block_name"": ""Jaoli"",
+      ""block_name_l"": """",
+      ""pincode"": 413608,
+      ""lat"": 28.7,
+      ""long"": 77.1,
+      ""from"": ""09:00:00"",
+      ""to"": ""18:00:00"",
+      ""fee_type"": ""Free"",
+      ""vaccine_fees"": [
+        {
+          ""vaccine"": ""##_Vaccine_##"",
+          ""fee"": ""250""
+        }
+      ],
+      ""sessions"": [
+      ]
+    }
+  ]
+}
+";
+            return rawJson
+                .Replace("##_HOSPITAL_##", hospitalName)
+                .Replace("##_Vaccine_##", vaccineType.ToString());
+        }
+
         public static string GetCentersApiResponseJson()
         {
             return @"
--- tests/Cowin.Watch.Core.Tests/WatchFunctionTests.cs
+++ tests/Cowin.Watch.Core.Tests/WatchFunctionTests.cs
@@ -1,9 +1,12 @@
 ﻿using Cowin.Watch.Core.Tests.Lib;
+using Cowin.Watch.Function.Config;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Cowin.Watch.Core.Tests
 {
@@ -11,18 +14,130 @@ namespace Cowin.Watch.Core.Tests
     class WatchFunctionTests
     {
         [TestMethod]
-        public void When_WatchFunction_Starts_Logging_Works()
+        public async Task When_WatchFunction_Starts_Logging_Works()
         {
+            var districtId = DistrictId.FromInt(56);
             var logger = GetLogger();
-            Cowin.Watch.Function.Watch.Run(null, logger);
+            string content = SampleJsonFactory.GetCentersApiResponseJson();
+            CowinApiHttpClient cowinApiClient = GetCowinApiClient(content);
+            var slotFinderByDistrictId = new SlotFinderByDistrictId(cowinApiClient, districtId);
+
+            await Cowin.Watch.Function.Watch.Run(null, logger, slotFinderByDistrictId);
+            var logs = (logger as ListLogger).Logs;
 
             Assert.IsNotNull(logger);
+            Assert.IsTrue(logs.Count > 0);
+            Assert.IsTrue(logs[1].Contains("Finding"));
+        }
 
+        private static CowinApiHttpClient GetCowinApiClient(string content)
+        {
+            return ClientFactory.GetHandlerFor_200(content) as CowinApiHttpClient;
+        }
+
+        [TestMethod]
+        public async Task When_WatchFunction_Starts_If_Api_Has_Slots_Result_Is_Logged()
+        {
+            var hospital = "Hosp";
+            var vaccine = VaccineType.Covaxin();
+            var vaccineCount = 10;
+            var sessionDate = DateTimeOffset.Now;
+            var districtId = DistrictId.FromInt(56);
+
+            var logger = GetLogger();
+            string content = SampleJsonFactory.GenerateResponseWithSessions(hospital, vaccine, vaccineCount, sessionDate);
+            CowinApiHttpClient cowinApiClient = GetCowinApiClient(content);
+            var slotFinderByDistrictId = new SlotFinderByDistrictId(cowinApiClient, districtId);
+
+            await Cowin.Watch.Function.Watch.Run(null, logger, slotFinderByDistrictId);
             var logs = (logger as ListLogger).Logs;
-            Assert.IsTrue( logs[1].Contains("Finding"));
 
+            var expectedLog = GetExpectedSuccessLog(hospital, vaccine, sessionDate, districtId);
+
+            Assert.IsTrue(logs.Any(line =>
+                line.Contains(expectedLog)));
+        }
+
+        [TestMethod]
+        public async Task When_WatchFunction_Starts_If_Api_Has_No_Slots_Result_Is_Logged()
+        {
+            var hospital = "Hosp";
+            var vaccine = VaccineType.Covaxin();
+            var districtId = DistrictId.FromInt(56);
+
+            var logger = GetLogger();
+            string content = SampleJsonFactory.GenerateResponseForHospitalAndVaccineWithoutSlots(hospital, vaccine);
+            CowinApiHttpClient cowinApiClient = GetCowinApiClient(content);
+            var slotFinderByDistrictId = new SlotFinderByDistrictId(cowinApiClient, districtId);
+
+            await Cowin.Watch.Function.Watch.Run(null, logger, slotFinderByDistrictId);
+            var logs = (logger as ListLogger).Logs;
+
+            var expectedLog = GetExpectedFailLog(hospital, vaccine, districtId);
+
+            Assert.IsTrue(logs.Any(line =>
+                line.Contains(expectedLog)));
+        }
+
+        [TestMethod]
+        public async Task When_WatchFunction_Starts_If_Api_Has_Slots_For_Vaccine_Result_Is_Logged()
+        {
+            var expectedVaccine = VaccineType.Covaxin();
+
+            var hospital = "Hosp";
+            var vaccineCount = 10;
+            var sessionDate = DateTimeOffset.Now;
+            var districtId = DistrictId.FromInt(56);
+
+            var logger = GetLogger();
+            string content = SampleJsonFactory.GenerateResponseWithSessions(hospital, expectedVaccine, vaccineCount, sessionDate);
+            CowinApiHttpClient cowinApiClient = GetCowinApiClient(content);
+            var slotFinderByDistrictId = new SlotFinderByDistrictId(cowinApiClient, districtId);
+
+            Environment.SetEnvironmentVariable(EnvVariables.KEY_SearchByVaccine, expectedVaccine.ToString());
+            await Cowin.Watch.Function.Watch.Run(null, logger, slotFinderByDistrictId);
+            var logs = (logger as ListLogger).Logs;
+
+            var expectedLog = GetExpectedSuccessLog(hospital, expectedVaccine, sessionDate, districtId);
+
+            Assert.IsTrue(logs.Any(line =>
+                line.Contains(expectedLog)));
+        }
+
+        [TestMethod]
+        public async Task When_WatchFunction_Starts_If_Api_Has_No_Slots_For_Vaccine_Result_Is_Logged()
+        {
+            var expectedVaccine = VaccineType.Covaxin();
+
+            var hospital = "Hosp";
+            var districtId = DistrictId.FromInt(56);
+
+            var logger = GetLogger();
+            string content = SampleJsonFactory.GenerateResponseForHospitalAndVaccineWithoutSlots(hospital, expectedVaccine);
+            CowinApiHttpClient cowinApiClient = GetCowinApiClient(content);
+            var slotFinderByDistrictId = new SlotFinderByDistrictId(cowinApiClient, districtId);
+
+            Environment.SetEnvironmentVariable(EnvVariables.KEY_SearchByVaccine, expectedVaccine.ToString());
+            await Cowin.Watch.Function.Watch.Run(null, logger, slotFinderByDistrictId);
+            var logs = (logger as ListLogger).Logs;
+
+            var expectedLog = GetExpectedFailLog(hospital, expectedVaccine, districtId);
+
+            Assert.IsTrue(logs.Any(line =>
+                line.Contains(expectedLog)));
+        }
+
+        private string GetExpectedFailLog(string hospital, VaccineType vaccine, DistrictId districtId)
+        {
+            return $"No slots for {vaccine} for districtId={districtId}";
+        }
+
+        private static string GetExpectedSuccessLog(string hospital, VaccineType vaccine, DateTimeOffset sessionDate, DistrictId districtId)
+        {
+            return $"Found slots for {vaccine} for districtId={districtId} at {hospital} on {sessionDate:d}";
         }
 
         private ILogger GetLogger() => new ListLogger();
+    }
 
 }
